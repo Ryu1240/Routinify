07faa61bb848d137091da4f485b723a723de1913
Ryusei Okuda <clz14026@ict.nitech.ac.jp>
2025-07-06 16:41:33 +0900

    コードクリーニング

--- backend/spec/support/authorization_helper.rb
+++ backend/spec/support/authorization_helper.rb
@@ -1,9 +1,4 @@
 module AuthorizationHelper
-  def authorization_stub
-    allow_any_instance_of(Secured).to receive(:authorize).and_return(true)
-    allow_any_instance_of(Secured).to receive(:current_user).and_return(current_user)
-  end
-
   def mock_controller_authentication(controller, user_id: 'test-user-id')
     # 認証をスキップ
     allow_any_instance_of(ApplicationController).to receive(:authorize).and_return(true)
@@ -17,14 +12,5 @@ module AuthorizationHelper
     
     # @decoded_tokenを設定
     controller.instance_variable_set(:@decoded_token, decoded_token)
-  end
-
-# Removed unused skip_authentication method
-
-# Removed unused enable_authentication method
-  private
-
-  def stub_authorize(klass)
-    allow_any_instance_of(klass).to receive(:authorize).and_return(true)
   end
 end
