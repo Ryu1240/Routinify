24ef648cc59c2d5beb5f780a1be9ca2c772067c0
Ryusei Okuda <clz14026@ict.nitech.ac.jp>
2025-10-21 22:02:01 +0900

    createアクションを追加

--- backend/app/controllers/api/v1/routine_tasks_controller.rb
+++ backend/app/controllers/api/v1/routine_tasks_controller.rb
@@ -8,6 +8,22 @@ module Api
         end
       end
 
+      def create
+        validate_permissions(['write:routine-tasks']) do
+          routine_task = RoutineTask.new(routine_task_params.merge(account_id: current_user_id))
+
+          if routine_task.save
+            render_success(
+              data: RoutineTaskSerializer.new(routine_task).as_json,
+              message: I18n.t('messages.routine_task.created', default: '習慣化タスクが正常に作成されました'),
+              status: :created
+            )
+          else
+            render_error(errors: routine_task.errors.full_messages)
+          end
+        end
+      end
+
       def show
         validate_permissions(['read:routine-tasks']) do
           routine_task = RoutineTask.find_by(id: params[:id], account_id: current_user_id)
@@ -18,6 +34,12 @@ module Api
             render_not_found('習慣化タスク')
           end
         end
+      end
+
+      private
+
+      def routine_task_params
+        params.require(:routine_task).permit(:title, :frequency, :interval_value, :next_generation_at, :max_active_tasks, :category_id, :priority, :is_active)
       end
     end
   end
