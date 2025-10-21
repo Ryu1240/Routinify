6b4d0094c80799bbbc7c3bc7939422cd963dfcae
Ryusei Okuda <clz14026@ict.nitech.ac.jp>
2025-10-21 09:57:27 +0900

    indexアクションの実装

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :tasks, only: [ :index, :show, :create, :update, :destroy ]
       resources :categories, only: [ :index, :create, :update, :destroy ]
+      resources :routine_tasks, only: [ :index ]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
