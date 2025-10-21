22b229edcd9a4edb7c2129fd85fdb5b262630163
Okuda Ryusei <128768127+Ryu1240@users.noreply.github.com>
2025-10-21 22:24:17 +0900

    showアクションを追加 (#141)
    Co-authored-by: Ryusei Okuda <clz14026@ict.nitech.ac.jp>

--- backend/app/controllers/api/v1/routine_tasks_controller.rb
+++ backend/app/controllers/api/v1/routine_tasks_controller.rb
@@ -7,6 +7,18 @@ module Api
           render_success(data: routine_tasks.map { |task| RoutineTaskSerializer.new(task).as_json })
         end
       end
+
+      def show
+        validate_permissions(['read:routine-tasks']) do
+          routine_task = RoutineTask.find_by(id: params[:id], account_id: current_user_id)
+
+          if routine_task
+            render_success(data: RoutineTaskSerializer.new(routine_task).as_json)
+          else
+            render_not_found('習慣化タスク')
+          end
+        end
+      end
     end
   end
 end
