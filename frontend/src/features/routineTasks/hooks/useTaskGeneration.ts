ac26e49b0d6153e386820e8297982990efba723f
Ryusei Okuda <clz14026@ict.nitech.ac.jp>
2025-10-27 00:48:49 +0900

    format

--- frontend/src/features/routineTasks/hooks/useTaskGeneration.ts
+++ frontend/src/features/routineTasks/hooks/useTaskGeneration.ts
@@ -94,9 +94,8 @@ export const useTaskGeneration = (): UseTaskGenerationReturn => {
         setGeneratedTasksCount(null);
 
         // タスク生成ジョブを開始
-        const job: TaskGenerationJob = await routineTasksApi.generate(
-          routineTaskId
-        );
+        const job: TaskGenerationJob =
+          await routineTasksApi.generate(routineTaskId);
 
         // ポーリング開始
         startPolling(routineTaskId, job.jobId);
