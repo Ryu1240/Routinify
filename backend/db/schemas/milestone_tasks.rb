a67015ef51714e0f4de8956a1de1dcead4e4653b
Ryusei Okuda <clz14026@ict.nitech.ac.jp>
2025-11-01 17:47:00 +0900

    index 追加

--- backend/db/schemas/milestone_tasks.rb
+++ backend/db/schemas/milestone_tasks.rb
@@ -2,5 +2,7 @@ create_table 'milestone_tasks', primary_key: [:milestone_id, :task_id], force: :
   t.integer 'milestone_id', null: false
   t.integer 'task_id',      null: false
 end
+
+add_index 'milestone_tasks', 'task_id'
 add_foreign_key 'milestone_tasks', 'milestones'
 add_foreign_key 'milestone_tasks', 'tasks'
