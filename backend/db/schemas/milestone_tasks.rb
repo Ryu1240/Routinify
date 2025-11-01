c29dfe08ecdd4166fcda5a07ffb668e966af5644
Ryusei Okuda <clz14026@ict.nitech.ac.jp>
2025-11-01 17:45:59 +0900

    複合キーの指定の仕方を修正

--- backend/db/schemas/milestone_tasks.rb
+++ backend/db/schemas/milestone_tasks.rb
@@ -1,8 +1,6 @@
-create_table 'milestone_tasks', id: false, force: :cascade do |t|
+create_table 'milestone_tasks', primary_key: [:milestone_id, :task_id], force: :cascade do |t|
   t.integer 'milestone_id', null: false
   t.integer 'task_id',      null: false
 end
-# 複合主キーを設定（PostgreSQLでは複合主キーが自動的にユニークインデックスも作成するため、ユニークインデックスの定義は不要）
-execute('ALTER TABLE milestone_tasks ADD PRIMARY KEY (milestone_id, task_id)')
 add_foreign_key 'milestone_tasks', 'milestones'
 add_foreign_key 'milestone_tasks', 'tasks'
