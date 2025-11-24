0df5feb2f8aed09224abb15fd62f1e2ca5e01d12
Ryusei Okuda <clz14026@ict.nitech.ac.jp>
2025-11-24 13:03:17 +0900

    cursorの指摘を修正

--- backend/bin/start.sh
+++ backend/bin/start.sh
@@ -4,7 +4,7 @@ set -e
 export RAILS_ENV=${RAILS_ENV:-production}
 
 # データベースマイグレーション（Ridgepole）
-bundle exec ridgepole --config ./config/database.yml -E production --file ./db/Schemafile --apply
+bundle exec ridgepole --config ./config/database.yml --env ${RAILS_ENV} --file ./db/Schemafile --apply
 
 # Railsサーバーを起動
 exec bundle exec rails server -p ${PORT:-3000} -b 0.0.0.0
