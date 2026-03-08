f6a66a066519873cbd6122597e8c5945c6f6cfc5
Ryusei Okuda <ryusei@RyuseinoMacBook-Air.local>
2026-03-08 21:31:01 +0900

    configの修正

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
+# テスト実行時は必ず test 環境を使用（コンテナの RAILS_ENV=development との競合を防ぐ）
+ENV['RAILS_ENV'] = 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
